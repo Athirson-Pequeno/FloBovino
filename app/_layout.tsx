b682f95a98d58007ff5472c6486bc609c5d24724
Athirson <athirsondev@gmail.com>
2025-10-29 08:48:35 -0300

    Mudança na cor da barra de navegação do android

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,8 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
+import * as NavigationBar from "expo-navigation-bar";
+import { useEffect } from "react";
 
 import { useColorScheme } from "@/hooks/use-color-scheme";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -13,23 +15,28 @@ export const unstable_settings = {
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
+  useEffect(() => {
+    NavigationBar.setBackgroundColorAsync("#f0f0f0");
+    NavigationBar.setButtonStyleAsync("dark");
+  }, []);
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <StatusBar style="auto" />
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" />
-        <Stack.Screen name="pages/login" />
-        <Stack.Screen name="pages/home" />
-        <Stack.Screen name="pages/loginFazendeiro" />
-        <Stack.Screen name="forms/formularioAnimal" />
-        <Stack.Screen name="forms/formularioFazendeiro" />
-        <Stack.Screen name="forms/formularioVeterinario" />
-        <Stack.Screen name="pages/fazendeiros" />
-        <Stack.Screen name="pages/veterinarios" />
-        <Stack.Screen name="pages/dashboardFazendeiro" />
-      </Stack>
-    </ThemeProvider>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <StatusBar style="auto" />
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" />
+          <Stack.Screen name="pages/login" />
+          <Stack.Screen name="pages/home" />
+          <Stack.Screen name="pages/loginFazendeiro" />
+          <Stack.Screen name="forms/formularioAnimal" />
+          <Stack.Screen name="forms/formularioFazendeiro" />
+          <Stack.Screen name="forms/formularioVeterinario" />
+          <Stack.Screen name="pages/fazendeiros" />
+          <Stack.Screen name="pages/veterinarios" />
+          <Stack.Screen name="pages/dashboardFazendeiro" />
+        </Stack>
+      </ThemeProvider>
     </SafeAreaView>
   );
 }
